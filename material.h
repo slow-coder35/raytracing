4bb787cd941c811ea8e0d00e485494608364d7f9
Prajwal Deshpande <deshpandeprajwal07@gmail.com>
2026-06-23 21:42:29 +0530

    canged the attenuation to take texture as assignments to it adn help it in texture building instad of direc color imposition

--- material.h
+++ material.h
@@ -5,6 +5,7 @@
 #include "rtweekend.h"
 #include "hittable.h"
 #include <iostream>
+#include "texture.h"
 
 
 class material{
@@ -16,19 +17,21 @@ class material{
 
 class lambertian :public material{
     public :
-        lambertian(const color& albedo ) : albedo(albedo) {}
+        lambertian(const color& albedo ) : tex(make_shared<solid_color>(albedo)) {}
+        lambertian(shared_ptr<texture> tex): tex(tex) {}
         bool scatter(const ray& r_in,const hit_record& rec ,color& attenuation,ray& scattered)const override{
             auto scatter_direction=rec.normal +random_unit_vector();
+            //dgen scatter direction 
             if(scatter_direction.near_zero())
                 scatter_direction=rec.normal;
 
             scattered=ray(rec.p,scatter_direction,r_in.time());
-            attenuation=albedo;
+            attenuation=tex->value(rec.u,rec.v,rec.p);
             return true;
         }
     
         private :
-        color albedo;
+        shared_ptr<texture> tex;
 };
 
 
