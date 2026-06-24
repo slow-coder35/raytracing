ebab38b60743003f7ecff0292584eb12e387435d
Prajwal Deshpande <deshpandeprajwal07@gmail.com>
2026-06-25 02:23:16 +0530

    implemented disk hittable

---
+++ disk.h
@@ -0,0 +1,76 @@
+#ifndef DISK_H
+#define DISK_H
+
+
+#include "aabb.h"
+#include "hittable.h"
+
+
+class disk :public hittable{
+    public:
+
+    disk(const point3& center,const vec3& normal,double radius,shared_ptr<material> mat) : center(center),normal(unit_vector(normal)),radius(radius),mat(mat){
+        auto rvec=vec3(radius,radius,radius);
+        bbox=aabb(center-rvec,center+rvec);
+    }
+
+    aabb bounding_box() const override {return bbox;}
+
+    bool hit(const ray& r,interval ray_t,hit_record& rec) const override {
+        double denom=dot(normal,r.direction());//denominator
+
+        if(std::fabs(denom)<1e-8) return false;   //is parallel to the plane
+        
+        auto t=(dot(center-r.origin(),normal))/denom;  //parameter t
+        if(!ray_t.contain(t)) return false;            //intersection is behind the camera
+
+        point3 p=r.at(t);
+
+        if((p-center).length_squared()>radius*radius)return false;
+            
+        
+        rec.t=t;
+        rec.p=r.at(t);
+        rec.mat=mat;
+        rec.set_face_normal(r,normal);
+            
+        return true;
+        
+    }  
+    
+    private:
+
+    point3 center;
+    vec3 normal;
+    double radius;
+    shared_ptr<material> mat;
+    aabb bbox;
+
+};
+
+// class ring:public disk{
+
+//     public:
+//         ring(const point3& center,const vec3& normal,double inner_radius,double outer_radius,shared_ptr<material> mat) : center(center),normal(unit_vector(normal)),inner_radius(inner_radius),outer_radius(outer_radius),mat(mat){
+//         auto rvec=vec3(outer_radius,outer_radius,outer_radius);
+//         bbox=aabb(center-rvec,center+rvec);
+//     }
+
+
+
+
+//     private:
+//         point3 center;
+//         vec3 normal;
+//         double inner_radius,outer_radius;
+//         shared_ptr<material> mat;
+//         aabb bbox;
+        
+
+// };
+
+
+
+
+
+#endif
