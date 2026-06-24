cb37587f1864f8aed2bc2460d392722ac534d988
Prajwal Deshpande <deshpandeprajwal07@gmail.com>
2026-06-24 22:06:32 +0530

    added marble texture

--- texture.h
+++ texture.h
@@ -83,7 +83,9 @@ public:
 noise_texture(double scale):scale(scale){}
 
 color value (double u,double v,const point3& p)const override{
-    return color(1,1,1)*0.5*( 1.0 + noise.noise(scale*p));
+    // return color(1,1,1)*0.5*( 1.0 + noise.noise(scale*p));
+    // return color(1,1,1)* noise.turb(p,7);  straight up  noise directly into the render
+    return color(0.5,0.5,0.5)* (1+std::sin(scale*p.z()+10*noise.turb(p,7)));  //hello world solid textures
 
 }
 private:
